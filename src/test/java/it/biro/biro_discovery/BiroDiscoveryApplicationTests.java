56563f5ea76e2d1f55c9c90be4b894ad604732ef
Davide del Vecchio <davide.delvecchio2@studio.unibo.it>
2022-11-06 18:15:01 +0100

    create discovery service

---
+++ src/test/java/it/biro/biro_discovery/BiroDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package it.biro.biro_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiroDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
